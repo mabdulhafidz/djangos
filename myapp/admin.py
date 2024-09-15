aa49af5a3c9decbba7b8ef197f1a860e0e8acb7a
M Abdul Hafidz <hfidz7534@gmail.com>
2024-09-16 03:32:36 +0700

    feat: fixed

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from.models import TodoItem
+# from.models import TodoItem
 
 # Register your models here.
-admin.site.register(TodoItem)
+# admin.site.register(TodoItem)
--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class TodoItem(models.Model):
-    title = models.CharField(max_Length=200)
-    completed = models.BooleanField(default=False)
+# class TodoItem(models.Model):
+#     title = models.CharField(max_Length=200)
+#     completed = models.BooleanField(default=False)
